c9b7cc476bc0427af4db07c2f935a3467143fd3e
Chulani Vimukthi <121760253+chula805@users.noreply.github.com>
2023-12-18 17:23:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const mixer = mixitup(".project__grid");
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
